34cc911efead4d65a9852a2da2ae9f9cf70b09a6
felixsuarez0727 <felixdavidsuarezbonilla@gmail.com>
2021-07-05 19:46:13 +0000

    Missed to commit this change

--- server/test/test_svc.sh
+++ server/test/test_svc.sh
@@ -1,1 +1,3 @@
 curl -i -X POST -H "Content-Type: application/json; indent=3" -d '{"method": "mining.authorize","params": ["11","12"],"id": "1"}' http://localhost:5001/api
+curl -i -X POST -H "Content-Type: application/json; indent=3" -d '{"method": "mining.subscribe","params": ["felix0727"],"id": "1"}' http://localhost:5001/api
+curl -i -X POST -H "Content-Type: application/json; indent=3" -d '{"method": "mining.notify","id": "1"}' http://localhost:5001/api
